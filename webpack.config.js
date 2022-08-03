ec6fab18dc1ed229d4e54617a2d43a5d0092b52e
Maksym Dmitriiev <maximdm22@gmail.com>
2022-08-03 18:27:58 +0300

    qqq

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,40 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  stats: "errors-only",
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+    
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        test: /\.scss$/i,
+        use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
       },
     ],
   },
+  plugins: [
+  new HtmlWebpackPlugin({ template: 'src/index.html' }),
+  new MiniCssExtractPlugin({
+
+    filename: "styles.css",
+    // chunkFilename: "[id].css",
+  }),
+  new CleanWebpackPlugin(),
+],
   devServer: {
     port: 4444,
     open: true,
-    stats: "errors-only",
   },
 };
